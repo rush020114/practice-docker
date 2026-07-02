c29be73bc20fe5abb5c813c189ddd692c8be7a7b
rush020114 <rush020114@gmail.com>
2026-07-03 08:56:47 +0900

    complete to connect db and fastanpi and nginx

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,19 @@
-# fastapi/main.py
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
+DATABASE_URL = f"postgresql://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@db:5432/{os.environ['POSTGRES_DB']}"
+
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
 def read_root():
-  return {"message": "Hello Docker!"}
+    return {"message": "Hello Docker!"}
+
+@app.get("/db-test")
+def db_test():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT 1"))
+    return {"db": "connected"}
